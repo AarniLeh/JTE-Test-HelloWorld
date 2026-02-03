971ae5579dfaf9bb17fe7112f9fd239e86993a5d
AarniLeh <126670806+AarniLeh@users.noreply.github.com>
2026-02-03 13:40:39 +0200

    Update and rename Main.java to Laskin.java

--- Laskin.java
+++ Laskin.java
@@ -1,5 +1,8 @@
-public class Main {
+public class Laskin {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+    public static int laskin(int a, int b) {
+        return a+b;
     }
 }
