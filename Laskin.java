4c72e130e9e09b6ddf5ff7a2db1ef8dd6d9f97bb
AarniLeh <126670806+AarniLeh@users.noreply.github.com>
2026-02-03 14:34:56 +0200

    Delete Laskin.java

--- Laskin.java
+++
@@ -1,8 +0,0 @@
-public class Laskin {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    public static int laskin(int a, int b) {
-        return a+b;
-    }
-}
