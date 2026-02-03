42a7d6f8ee6b8afadf7e10da8770482f01ec9ed0
AarniLeh <126670806+AarniLeh@users.noreply.github.com>
2026-02-03 14:01:14 +0200

    Update Test_laskin.java

--- Test_laskin.java
+++ Test_laskin.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Test_laskin {
     @Test
-    void main() {
+    void LaskinTest() {
         assertEquals(5, Laskin.laskin(3, 2));
     }
 }
