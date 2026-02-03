109a84102a7fa24c5a34950c12d0452b1d026c55
AarniLeh <126670806+AarniLeh@users.noreply.github.com>
2026-02-03 14:21:46 +0200

    Update Laskin.java

--- src/test/java/Laskin.java
+++ src/test/java/Laskin.java
@@ -1,1 +1,8 @@
-
+public class Laskin {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    public static int laskin(int a, int b) {
+        return a+b;
+    }
+}
